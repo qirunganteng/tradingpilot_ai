35c82baf95f8ac8a9693908ca331654ec591dd95
qirunganteng <bankerdomino@gmail.com>
2026-07-26 07:41:04 +0700

    Fix: Add layout weight import in AddTradeScreen.kt

--- TradePilotAI/feature/feature-journal/src/main/java/com/tradepilot/feature/journal/AddTradeScreen.kt
+++ TradePilotAI/feature/feature-journal/src/main/java/com/tradepilot/feature/journal/AddTradeScreen.kt
@@ -3,6 +3,7 @@ package com.tradepilot.feature.journal
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope.weight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
